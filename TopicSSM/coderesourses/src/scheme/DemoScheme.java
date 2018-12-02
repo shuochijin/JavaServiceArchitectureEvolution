a18e45b8aecc437f24c3545f62175db3a9f719b3
shuochijin <835391854@163.com>
2018-12-02 16:50:09 +0800

    Create DemoScheme.java

---
+++ TopicSSM/coderesourses/src/scheme/DemoScheme.java
@@ -0,0 +1,48 @@
+package scheme;
+
+public class DemoScheme {
+	
+	private int id = -1;
+	
+	private String name = "";
+	
+	private String value = "";
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		s.append(this.getId());
+		s.append(",");
+		s.append(this.getName());
+		s.append(",");
+		s.append(this.getValue());
+		s.append("]");
+		return s.toString();
+	}
+	
+}
