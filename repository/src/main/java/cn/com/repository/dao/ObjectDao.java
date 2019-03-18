7b77d19a0220d62b08f8c9654536f66c9568efea
shuochijin <835391854@163.com>
2019-03-18 22:08:30 +0800

    Create ObjectDao.java

---
+++ repository/src/main/java/cn/com/repository/dao/ObjectDao.java
@@ -0,0 +1,32 @@
+package cn.com.repository.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import cn.com.repository.entity.ObjectEntity;
+
+public interface ObjectDao {
+
+	@Mapper
+	public ObjectEntity getDataById(int id);
+
+	@Mapper
+	public List<ObjectEntity> queryAll();
+
+	@Mapper
+	public List<ObjectEntity> queryPage(Map map);
+
+	@Mapper
+	public int insert(ObjectEntity entity);
+	
+	@Mapper
+	public int updateById(ObjectEntity entity);
+	
+	@Mapper
+	public int deleteById(ObjectEntity entity);
+	
+	@Mapper
+	public int deleteByIds(List list);
+}
