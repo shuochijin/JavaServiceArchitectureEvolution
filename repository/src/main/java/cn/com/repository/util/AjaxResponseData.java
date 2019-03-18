3fd223574715534c0f213964053ce9aef1fa66ae
shuochijin <835391854@163.com>
2019-03-18 22:08:38 +0800

    Create AjaxResponseData.java

---
+++ repository/src/main/java/cn/com/repository/util/AjaxResponseData.java
@@ -0,0 +1,25 @@
+package cn.com.repository.util;
+
+public class AjaxResponseData {
+	
+	private String isSuccess;
+	
+	private Object data;
+
+	public String getIsSuccess() {
+		return isSuccess;
+	}
+
+	public void setIsSuccess(String isSuccess) {
+		this.isSuccess = isSuccess;
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+}
