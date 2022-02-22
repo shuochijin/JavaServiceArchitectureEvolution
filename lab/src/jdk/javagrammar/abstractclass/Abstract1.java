58af086403f01a0fd4d5ee2d8a93070e47eeaeaf
shuochijin <835391854@163.com>
2022-02-22 17:52:28 +0800

    20220222

---
+++ lab/src/jdk/javagrammar/abstractclass/Abstract1.java
@@ -0,0 +1,20 @@
+package jdk.javagrammar.abstractclass;
+
+public class Abstract1 extends TestAbstract {
+
+	@Override
+	public int create() {
+		return 0;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("Abstract1");
+	}
+
+	@Override
+	public void printCreate() {
+		
+	}
+
+}
